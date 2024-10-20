524acfc8eb199212aec77f9b23bc49bd5eb9fd12
Piotr Wysocki <wysockip14@gmail.com>
2024-10-20 18:34:42 +0200

    Initial commit

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace ModTheCube {
+    public class Cube : MonoBehaviour
+    {
+        public MeshRenderer Renderer;
+        public float rotationSpeed = 10.0f;
+        public float rotationAngle = 5.0f;
+        public float scale = 1.3f;
+        public Color color = Color.red;
+        public int maxPosition = 6;
+        private Material material;
+        private float moveSpeed = 5f;
+
+        void Start()
+        {
+            var xPos = Random.Range(0, maxPosition);
+            var yPos = Random.Range(0, maxPosition);
+            var zPos = Random.Range(0, maxPosition);
+            transform.position = new Vector3(xPos, yPos, zPos);
+            material = Renderer.material;
+            InvokeRepeating(nameof(ChangeDirection), 2f, 5f);
+            InvokeRepeating(nameof(ChangeColor), 0f, 3f);
+            InvokeRepeating(nameof(ChangeScale), 0f, 1f);
+        }
+
+        private void ChangeDirection()
+        {
+            moveSpeed = -moveSpeed;
+        }
+        private void ChangeColor()
+        {
+            Color randomColor = Random.ColorHSV();
+            color = randomColor;
+        }
+        private void ChangeScale()
+        {
+            scale = Random.Range(1, 5);
+        }
+
+        void Update()
+        {
+            transform.Rotate(rotationSpeed * Time.deltaTime, rotationAngle, -rotationAngle);
+            material.color = color;
+            transform.localScale = Vector3.one * scale;
+            transform.Translate(Vector3.forward * (Time.deltaTime * moveSpeed));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Mod-The-Cube
+ unity unit 2 mod the cube project
